7c1b4ab295d0c11e593f94ca529788b5a78488f3
macrwfuse <132901460+macrwfuse@users.noreply.github.com>
2026-06-03 16:04:20 +0800

    Add server/routes/opengraph.js

---
+++ server/routes/opengraph.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import passwordWrapper from '../middlewares/passwordWrapper.js';
+import generateOpenGraphImage from '../controller/opengraph.js';
+
+const app = express.Router();
+
+app.get("/image", passwordWrapper(true, (req, res) => {
+  // If there is a password set and the user does not want others to view their test data, return the project banner
+  res.redirect('https://repository-images.githubusercontent.com/478222232/b5331514-aa27-4a56-af3e-c4b25446438d');
+}), async (req, res) => {
+
+  try {
+    const svg = await generateOpenGraphImage(req);
+
+    if (!svg) {
+      return res.status(500).json({ message: "Error fetching test data" });
+    }
+
+    res.setHeader("Content-Type", "image/svg+xml").status(200).send(svg);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default app;
