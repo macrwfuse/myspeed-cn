fd1390527dc9406b4084731564f9d66f755b4878
macrwfuse <132901460+macrwfuse@users.noreply.github.com>
2026-06-03 20:55:50 +0800

    feat: add XXIR_SERVERS to controller

--- server/controller/servers.js
+++ server/controller/servers.js
@@ -1,17 +1,27 @@
 import fs from 'node:fs';
-import { XXIR_SERVERS } from '../util/loadServers.js';
 
-// Embedded CN speedtest nodes (from spiritLHLS/speedtest.net-CN-ID)
-const CN_SERVERS = {
-    "5396": {"name":"Suzhou","sponsor":"China Telecom JiangSu 5G","country":"China","cc":"CN","distance":0,"host":"4gsuzhou1.speedtest.jsinfo.net.prod.hosts.ooklaserver.net"},
-    "16204": {"name":"Suzhou","sponsor":"JSQY - Suzhou","country":"China","cc":"CN","distance":0,"host":"speedtest.jsqiuying.com"},
-    "17265": {"name":"Zhonghe","sponsor":"FarEasTone Telecom","country":"Taiwan","cc":"CN","distance":0,"host":"fetsz1.seed.net.tw.prod.hosts.ooklaserver.net"},
-    "24447": {"name":"Shanghai","sponsor":"China Unicom 5G","country":"China","cc":"CN","distance":0,"host":"mobile.shunicomtest.com.prod.hosts.ooklaserver.net"},
-    "30852": {"name":"Kunshan","sponsor":"Duke Kunshan University","country":"China","cc":"CN","distance":0,"host":"speedtest.dukekunshan.edu.cn"},
-    "36663": {"name":"Zhenjiang","sponsor":"China Telecom JiangSu 5G","country":"China","cc":"CN","distance":0,"host":"5gzhenjiang.speedtest.jsinfo.net.prod.hosts.ooklaserver.net"},
-    "43752": {"name":"Beijing","sponsor":"BJ Unicom","country":"China","cc":"CN","distance":0,"host":"beijing.unicomtest.com"},
-    "59387": {"name":"NingBo","sponsor":"浙江电信","country":"China","cc":"CN","distance":0,"host":"cesu-nb.zjtelecom.com.cn.prod.hosts.ooklaserver.net"},
-    "73010": {"name":"Helan","sponsor":"Arslan Telecom","country":"Pakistan","cc":"CN","distance":0,"host":"arslantel.online.prod.hosts.ooklaserver.net"}
+// xxir CDN-based speed test nodes (speed.xxir.com)
+export const XXIR_SERVERS = {
+    "xxir-1": {
+        name: "CDN节点1 (国内多源)",
+        sponsor: "speed.xxir.com",
+        country: "China",
+        cc: "CN",
+        distance: 0,
+        host: "speed.xxir.com",
+        type: "xxir",
+        description: "30个国内CDN源随机下载测速（抖音/京东/百度/阿里等）"
+    },
+    "xxir-2": {
+        name: "CDN节点2 (分组随机)",
+        sponsor: "speed.xxir.com",
+        country: "China",
+        cc: "CN",
+        distance: 0,
+        host: "speed.xxir.com",
+        type: "xxir",
+        description: "7组CDN源随机选择一组后随机下载测速"
+    },
 };
 
 let ooklaServers;
@@ -40,9 +50,8 @@ export const getOoklaServers = () => {
         } catch { }
     }
 
-    // Always merge CN servers (CN servers take priority), then xxir nodes
-    servers = { ...servers, ...CN_SERVERS, ...XXIR_SERVERS };
-    ooklaServers = servers;
+    // Merge xxir nodes (xxir takes priority)
+    ooklaServers = { ...servers, ...XXIR_SERVERS };
 
     return ooklaServers;
 }
@@ -50,4 +59,5 @@ export const getOoklaServers = () => {
 export const getByMode = (mode) => {
     if (mode === "ookla") return getOoklaServers();
     if (mode === "libre") return getLibreServers();
+    if (mode === "xxir") return getOoklaServers(); // xxir nodes are in ookla list
 }
