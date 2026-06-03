f33c9a3e90f806d1c093f44768936a67500eeb08
macrwfuse <132901460+macrwfuse@users.noreply.github.com>
2026-06-03 20:56:29 +0800

    sync: error handler

---
+++ server/util/errorHandler.js
@@ -0,0 +1,15 @@
+import fs from "node:fs";
+const filePath = process.cwd() + "/data/logs/error.log";
+
+export default (error) => {
+    const date = new Date().toLocaleString();
+    const lineStarter = fs.existsSync(filePath) ? "\n\n" : "# Found a bug? Report it here: https://github.com/gnmyt/myspeed/issues\n\n";
+
+    console.error("An error occurred: " + error.message);
+
+    fs.writeFile(filePath, lineStarter + "## " + date + "\n" + error, {flag: 'a+'}, err => {
+        if (err) console.error("Could not save error log file.", error);
+
+        process.exit(1);
+    });
+};
